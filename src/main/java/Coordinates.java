9f277584aec90325ed6a8b3662c893f2c2c4f550
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-02 17:13:22 +0200

    Grid now handles edge cases and added more tests

---
+++ src/main/java/Coordinates.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alebaffa on 02/10/16.
+ */
+public class Coordinate {
+    static int x;
+    static int y;
+
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
